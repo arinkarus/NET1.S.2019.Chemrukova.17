a2b89b67413e7f9ce31cc23b68bd9d22eac9383b
arinkarus <arinkarus@gmail.com>
2019-04-28 15:15:35 +0300

    Console app added.

---
+++ WeatherStationViaInterfaces.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStationViaInterfaces.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var weatherData = new WeatherData();
+            var stats = new StatisticReport();
+            var currentReport = new CurrentConditionsReport();
+            weatherData.Register(currentReport);
+            weatherData.Register(stats);
+            weatherData.StartGettingMeasurements(1000);
+            System.Console.ReadLine();
+            weatherData.StopGettingMeasurements();
+            System.Console.ReadLine();
+            weatherData.Unregister(currentReport);
+            weatherData.StartGettingMeasurements(1000);
+            System.Console.ReadLine();
+            weatherData.StopGettingMeasurements();
+        }
+    }
+}
